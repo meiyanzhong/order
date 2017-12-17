a0b40aa0be2346aae3cb321fcf5e9b00ff798b9c
dengb <dengbin19910916@163.com>
2017-12-17 22:59:49 +0800

    Initial commit

---
+++ order-puller/src/main/java/com/topscore/omnichannel/OrderPullerApplication.java
@@ -0,0 +1,12 @@
+package com.topscore.omnichannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPullerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPullerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/topscore/omnichannel/OrderApplication.java
@@ -0,0 +1,12 @@
+package com.topscore.omnichannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+}
