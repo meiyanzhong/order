d1c17863406202fa420fd633e134a8ce57fa3ee2
dengb <dengbin19910916@163.com>
2017-12-18 10:34:28 +0800

    init

--- order-puller/src/main/java/com/topscore/omnichannel/order/GoodsController.java
+++ order-puller/src/main/java/com/topscore/omnichannel/order/GoodsController.java
@@ -1,12 +1,17 @@
 package com.topscore.omnichannel.order;
 
 import com.topscore.omnichannel.order.entity.Goods;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Api(tags = "商品服务")
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
@@ -14,9 +19,10 @@ public class GoodsController {
     @Autowired
     private GoodsMapper mapper;
 
-    @GetMapping
+    @ApiOperation("获取商品10001")
+    @GetMapping("/{number}")
     @ResponseBody
-    public Goods index() {
-        return mapper.findOne("10001");
+    public Goods index(@ApiParam(value = "商品编号", required = true) @PathVariable("number") String number) {
+        return mapper.findOne(number);
     }
 }
