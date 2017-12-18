4c0acdd787b8416c2cc058f9bd628e8337a3baf4
dengb <dengbin19910916@163.com>
2017-12-18 10:11:12 +0800

    init

--- order-puller/src/main/java/com/topscore/omnichannel/order/config/AppConfig.java
+++ order-puller/src/main/java/com/topscore/omnichannel/order/config/AppConfig.java
@@ -20,7 +20,7 @@ public class AppConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.topscore.omnichannel.order.site"))
+                .apis(RequestHandlerSelectors.basePackage("com.topscore.omnichannel.order"))
                 .paths(PathSelectors.any())
                 .build();
     }
