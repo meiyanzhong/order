6697a0edcd18a5cc52b82baaab0055e4a4af5e9e
dengb <dengbin19910916@163.com>
2017-12-19 21:56:58 +0800

    init

---
+++ order-place/src/main/java/com/topscore/omnichannel/order/orderplace/OrderPlaceApplication.java
@@ -0,0 +1,12 @@
+package com.topscore.omnichannel.order.orderplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPlaceApplication.class, args);
+	}
+}
---
+++ order-place/src/test/java/com/topscore/omnichannel/order/orderplace/OrderPlaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.topscore.omnichannel.order.orderplace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderPlaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
